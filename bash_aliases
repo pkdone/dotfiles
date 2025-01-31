820ac191b2dc921211b1a40054a35a3417a0ef92
Paul Done <pkdone@gmail.com>
2025-01-31 16:08:43 +0000

    added fish backspace delete word keybinding

--- bash_aliases
+++ bash_aliases
@@ -2,4 +2,3 @@ alias rm='rm -i'
 alias mv='mv -i'
 alias pycodestyle='pycodestyle --max-line-length=100'
 alias publicip="curl -s ifconfig.me && echo"
-
--- config.fish
+++ config.fish
@@ -3,4 +3,4 @@ alias rm 'rm -i'
 alias mv 'mv -i'
 alias pycodestyle 'pycodestyle --max-line-length=100'
 alias publicip "curl -s ifconfig.me && echo"
-
+bind \cH backward-kill-word
